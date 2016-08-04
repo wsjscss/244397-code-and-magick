a20a393c53df932828229fbb3407a5a1d7156412
Denis <top.muk@mail.ru>
2016-08-04 22:32:37 +0300

    remove jsDoc on if(){} . {unknown} ==> {*}

--- build/js/check.js
+++ build/js/check.js
@@ -1,14 +1,12 @@
 /**
  * Ф-ция получает данные, проверяет их тип и позвращает сообщение
- * @param {неопределенно} a - первое значение
- * @param {неопределенно} b - второе хначение
+ * @param {*} a - первое значение
+ * @param {*} b - второе хначение
  */
 
 function getMessage(a, b){
 
-  /**
-   * Если первый аргумент, a, имеет тип boolean
-   */
+
   if (typeof a === 'boolean') {
     if (a) {                                  //Если он true
       return 'Я попал в ' + b;
@@ -18,26 +16,17 @@ function getMessage(a, b){
   }
 
 
-  /**
-   * Если первый аргумент имеет числовой тип
-   */
   if (typeof a === 'number'){
     return 'Я прыгнул на ' + a * 100 + ' сантиметров';
   }
 
 
-  /**
-   * Если первый аргумент массив
-   */
   if (Array.isArray(a) && !Array.isArray(b)){
     var numberOfSteps = summArrays(a);
     return 'Я прошёл '+ numberOfSteps +' шагов';
   }
 
 
-  /**
-   * Если оба аргумента массивы
-   */
   if (Array.isArray(a) && Array.isArray(b)) {
     var distancePath = summArrays(a) + summArrays(b)
     return 'Я прошёл '+ distancePath +' метров'
