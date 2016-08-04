be32f4e2b2eec433d6a803cb31156428d622bbb9
Denis <top.muk@mail.ru>
2016-08-05 01:30:37 +0300

    Install Eslint =D

--- build/js/check.js
+++ build/js/check.js
@@ -1,50 +1,52 @@
-/**
- * Ф-ция получает данные, проверяет их тип и позвращает сообщение
- * @param {*} a - первое значение
- * @param {*} b - второе хначение
- */
+'use strict';
 
+/**
+* Получение 2 переменных и проверка их типов
+* В зависимости от типа переменных выводится сообщение
+* @param {*} a
+* @param {*} b
+*/
 function getMessage(a, b){
 
-
   if (typeof a === 'boolean') {
-    if (a) {                                  //Если он true
+    if (a) {
       return 'Я попал в ' + b;
-    } else {                                  //Если он false
+    } else {
       return 'Я никуда не попал';
     }
   }
-
 
   if (typeof a === 'number'){
     return 'Я прыгнул на ' + a * 100 + ' сантиметров';
   }
 
-
   if (Array.isArray(a) && !Array.isArray(b)){
-    var numberOfSteps = summArrays(a);
-    return 'Я прошёл '+ numberOfSteps +' шагов';
-  }
+    var numberOfSteps = a.reduce(function (sum, current) {
+      return sum + current;
+    });
 
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
+  }
 
   if (Array.isArray(a) && Array.isArray(b)) {
-    var distancePath = summArrays(a) + summArrays(b)
-    return 'Я прошёл '+ distancePath +' метров'
+    var distancePath = calculatePath(a, b);
+    
+    return 'Я прошёл ' + distancePath + ' метров';
+  }
+}
+
+/**
+* Попарно перемножаем значения из 2 массивов и складываем полученные результаты
+* @param {Array} - arr
+* @param {Array} - arr
+* @return {Number}
+*/
+function calculatePath(a, b) {
+  var result = 0;
+
+  for (var i = 0; i < a.length; i++) {
+    result = result + a[i] * b[i];
   }
 
-
-  /**
-   * Calculate sum of all array elements
-   * @param {Array} - arr
-   * @return sum of all Array elements
-   */
-  function summArrays(arr) {
-    var result = 0
-
-    arr.forEach(function (a) {
-      result += a
-    })
-
-    return result
-  };
-};
+  return result;
+}
