00880a3751e98ed40ba7cd4434550035a2fe3401
Denis <top.muk@mail.ru>
2016-08-13 00:40:02 +0300

    preparing module3-task1

---
+++ src/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+require('./form.js');
+require('./check.js');
+require('./game-stat.min.js');
+
+(function() {
+  var game = new window.Game(document.querySelector('.demo'));
+  game.initializeLevelAndStart();
+  game.setGameStatus(window.Game.Verdict.INTRO);
+
+  var formOpenButton = document.querySelector('.reviews-controls-new');
+
+  /** @param {MouseEvent} evt */
+  formOpenButton.onclick = function(evt) {
+    evt.preventDefault();
+
+    window.form.open(function() {
+      game.setGameStatus(window.Game.Verdict.PAUSE);
+      game.setDeactivated(true);
+    });
+  };
+
+  window.form.onClose = function() {
+    game.setDeactivated(false);
+  };
+})();
